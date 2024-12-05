80c59e0cad214ab81b919f21a30c3895cc221e93
kyddaniel <mksuenaa@connect.ust.hk>
2024-12-05 22:21:19 +0800

    Implement Aspect (After Advice)

--- src/main/java/com/kyddaniel/JobApp/aop/LoggingAspect.java
+++ src/main/java/com/kyddaniel/JobApp/aop/LoggingAspect.java
@@ -1,8 +1,7 @@
 package com.kyddaniel.JobApp.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -19,8 +18,23 @@ public class LoggingAspect {
         LOGGER.info("Method called");
     }*/
 
-    @Before("execution(* com.kyddaniel.JobApp.service.JobService.getAllJobs(..))")
+    @Before("execution(* com.kyddaniel.JobApp.service.JobService.*(..))")
     public void logMethodCall(JoinPoint joinPoint) {
-        LOGGER.info("Method called {}", joinPoint.getSignature().getName());
+        LOGGER.info("Method called : {}()", joinPoint.getSignature().getName());
+    }
+
+    @After("execution(* com.kyddaniel.JobApp.service.JobService.*(..))")
+    public void logMethodExecuted(JoinPoint joinPoint) {
+        LOGGER.info("Method executed : {}()", joinPoint.getSignature().getName());
+    }
+
+    @AfterReturning("execution(* com.kyddaniel.JobApp.service.JobService.*(..))")
+    public void logMethodExecuteSuccess(JoinPoint joinPoint) {
+        LOGGER.info("Method {}() executed successfully", joinPoint.getSignature().getName());
+    }
+
+    @AfterThrowing("execution(* com.kyddaniel.JobApp.service.JobService.*(..))")
+    public void logMethodCrash(JoinPoint joinPoint) {
+        LOGGER.info("Method has some issues : {}()", joinPoint.getSignature().getName());
     }
 }
